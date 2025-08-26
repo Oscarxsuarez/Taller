cdcbfdface85a350bf7248953437a319f4775d46
Oscarxsuarez <oscar252423@gmail.com>
2025-08-26 18:31:19 -0500

    Update and rename Situacion 2.cpp to validacion_fecha.cpp

--- src/validacion_fecha.cpp
+++ src/validacion_fecha.cpp
@@ -5,12 +5,6 @@ using namespace std;
 int main (){
 	
 	int mesact, mes, ano, dia;
-	float total;
-	
-	
-	//cout<<"Digita el total a pagar": ; 
-	
-	
 	cout<<"Digite el mes actual (en numeros, ejemplo: 4, es decir abril )\n"; cin>>mesact;
 	cout<<"Digite su ano de nacimiento(tienes que por lo menos haber nacido en 2007)"<<endl; cin>>ano;
 	cout<<"Digite su mes de nacimiento (en numeros, ejemplo: 4, es decir abril )"<<endl; cin>>mes;
@@ -19,7 +13,7 @@ int main (){
 	if (ano<2008  && mesact==mes  && dia<=31) {
 		switch(mesact){
 			case 1 :
-				cout<<"¡Tienes la promocion especial por cumplir en Enero!\n";
+				cout<<"Â¡Tienes la promocion especial por cumplir en Enero!\n";
 				break;
 			case 2 :
 				if(dia>29){
@@ -27,20 +21,20 @@ int main (){
 					break;
 				}
 				
-			cout<<"¡Tienes la promocion especial por cumplir en Febrero!\n";
+			cout<<"Â¡Tienes la promocion especial por cumplir en Febrero!\n";
 				break;
 			case 3 :
-				cout<<"¡Tienes la promocion especial por cumplir en Marzo!\n";
+				cout<<"Â¡Tienes la promocion especial por cumplir en Marzo!\n";
 				break;
 			case 4 :
 					if(dia>30){
 					cout<<"Error\n";
 					break;
 				}
-				cout<<"¡Tienes la promocion especial por cumplir en Abril!\n";
+				cout<<"Â¡Tienes la promocion especial por cumplir en Abril!\n";
 				break;
 			case 5 :
-				cout<<"¡Tienes la promocion especial por cumplir en Mayo!\n";
+				cout<<"Â¡Tienes la promocion especial por cumplir en Mayo!\n";
 				
 				break;
 			case 6 :
@@ -48,45 +42,39 @@ int main (){
 					cout<<"Error\n";
 					break;
 				}					
-				cout<<"¡Tienes la promocion especial por cumplir en Junio!\n";
+				cout<<"Â¡Tienes la promocion especial por cumplir en Junio!\n";
 				break;
 			case 7 :
-				cout<<"¡Tienes la promocion especial por cumplir en Julio!\n";
+				cout<<"Â¡Tienes la promocion especial por cumplir en Julio!\n";
 				break;
 			case 8 :
-				cout<<"¡Tienes la promocion especial por cumplir en Agosto!\n";
+				cout<<"Â¡Tienes la promocion especial por cumplir en Agosto!\n";
 				break;
 			case 9 :
 					if(dia>30){
 					cout<<"Error\n";
 					break;
 				}	
-				cout<<"¡Tienes la promocion especial por cumplir en Septiembre!\n";
+				cout<<"Â¡Tienes la promocion especial por cumplir en Septiembre!\n";
 				break;
 			case 10 :
-				cout<<"¡Tienes la promocion especial por cumplir en Octubre!\n";
+				cout<<"Â¡Tienes la promocion especial por cumplir en Octubre!\n";
 				break;
 			case 11 :
 					if(dia>30){
 					cout<<"Error\n";
 					break;
 				}	
-				cout<<"¡Tienes la promocion especial por cumplir en Noviembre!\n";
+				cout<<"Â¡Tienes la promocion especial por cumplir en Noviembre!\n";
 				break;
 			case 12 :
-				cout<<"¡Tienes la promocion especial por cumplir en Diciembre!\n";
+				cout<<"Â¡Tienes la promocion especial por cumplir en Diciembre!\n";
 				break;
 			default :
 				cout<<"Error, no se permite esta fecha\n";
-			
-				
-			
-			
-	
 		}
 		
 	}
-	
 	else if (mesact != mes){
 		cout<<"Lo siento, no tienes la promocion\n";
 	}
@@ -94,10 +82,6 @@ int main (){
 	cout<<"Error, no se permite esta fecha\n";
 	}
 	cout<<"Las fechas ingresadas son del ano: "<<ano<<" del mes "<<mes<<" del dia "<<dia<<" y el mes actual para la promocion es el : "<<mesact;
-	
-	
-	
-		
-	
 	return 0;
 }
+
